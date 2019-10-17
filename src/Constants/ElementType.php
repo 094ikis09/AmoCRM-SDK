753324cb1330600c20d5d34e3de4957fde16638f
Валерий <094ikis09@gmaol.com>
2019-10-17 13:51:49 +0300

    add constants

---
+++ src/Constants/ElementType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AmoCRM\Constats;
+
+class ElementType
+{
+   /**
+    * Тип элемента: Контакт
+    */
+   const CONTACT = 1;
+   /**
+    * Тип элемента: Сделка
+    */
+   const LEAD = 2;
+   /**
+    * Тип элемента: Компания
+    */
+   const COMPANY = 3;
+   /**
+    * Тип элемента: Покупатель
+    */
+   const CUSTOMER = 12;
+   /**
+    * Тип элемента: Без типа
+    */
+   const NONE = 0;
+}
