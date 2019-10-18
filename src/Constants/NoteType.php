9160bcdd4b7ec38629451e1cb8503b05e9fc2bc7
Валерий <094ikis09@gmaol.com>
2019-10-18 13:06:36 +0300

    Add Note Type

---
+++ src/Constants/NoteType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AmoCRM\Constats;
+
+class NoteType
+{
+   /**
+    * Обычное примечание
+    */
+   const COMMON = 4;
+
+   /**
+    * Результат по задаче
+    */
+   const TASK_RESULT = 13;
+
+   /**
+    * Системное сообщение
+    */
+   const SYSTEM = 25;
+
+   /**
+    * Входящее смс
+    */
+   const SMS_IN = 102;
+
+   /**
+    * Исходящее смс
+    */
+   const SMS_OUT = 103;
+}
