19e1a065a15e4f6842e03e633889734df39449a9
Валерий <094ikis09@gmaol.com>
2019-10-18 13:05:47 +0300

    Release Status Entity

--- src/Entities/StatusEntity.php
+++ src/Entities/StatusEntity.php
@@ -2,50 +2,170 @@
 
 namespace AmoCRM\Entities;
 
+use AmoCRM\Exceptions\AmoCRMException;
+
+/**
+ * Сущность этапа
+ */
 class StatusEntity extends BaseEntity
 {
 
-   protected $pipeline_id;
-   protected $name;
-   protected $color;
-   protected $sort = 0;
-   protected $is_editable;
+   protected
+      $pipeline_id,
+      $name,
+      $color,
+      $sort = 0,
+      $is_editable;
 
    public function __construct($entity = null, $pipeline_id, $sort = null)
    {
+      if (!is_numeric($pipeline_id)) {
+         throw new AmoCRMException('Передаваемая переменная не является числом');
+      }
       $this->pipeline_id = $pipeline_id;
+
       if (is_array($entity)) {
+
+         if (!is_numeric($entity['id'])) {
+            throw new AmoCRMException('Передаваемая переменная не является числом');
+         }
          $this->id = $entity['id'];
+
+         if (!is_string($entity['name'])) {
+            throw new AmoCRMException('Передаваемая переменная не является строкой');
+         }
          $this->name = $entity['name'];
+
+         if (!is_string($entity['color'])) {
+            throw new AmoCRMException('Передаваемая переменная не является строкой');
+         }
          $this->color = $entity['color'];
+
+         if (!is_numeric($entity['sort'])) {
+            throw new AmoCRMException('Передаваемая переменная не является числом');
+         }
          $this->sort = $entity['sort'];
+
+         if (!is_bool($entity['is_editable'])) {
+            throw new AmoCRMException('Передаваемая переменная не является булевой');
+         }
          $this->is_editable = $entity['is_editable'];
       } else {
-         if ($sort !== null)
+         if ($sort !== null) {
+            if (!is_numeric($sort)) {
+               throw new AmoCRMException('Передаваемая переменная не является числом');
+            }
             $this->sort = ($sort) * 10;
+         }
       }
    }
 
+   /**
+    * Получить уникальный индетификатор родительской воронки
+    * @return int
+    */
+   public function getPipelineId()
+   {
+      return $this->pipeline_id;
+   }
+
+   /**
+    * Получить название этапа
+    * @return string
+    */
    public function getName()
    {
       return $this->name;
    }
 
+   /**
+    * Задать название этапа
+    * @param string $name
+    * @return  self
+    */
    public function setName($name)
    {
+      if (!is_string($name)) {
+         throw new AmoCRMException('Передаваемая переменная не является строкой');
+      }
+
       $this->name = $name;
+
       return $this;
    }
 
-   public function setSort($sort)
+   /**
+    * Получить цвет этапа
+    * @return string
+    */
+   public function getColor()
    {
-      $this->sort = $sort;
-      return $this;
+      return $this->color;
    }
 
+   /**
+    * Задать цвет этапа
+    * @param string $color
+    * @return  self
+    */
    public function setColor($color)
    {
+      if (!is_string($color)) {
+         throw new AmoCRMException('Передаваемая переменная не является строкой');
+      }
+
       $this->color = $color;
+
+      return $this;
+   }
+
+   /**
+    * Получить порядковый номер этапа
+    * @return int
+    */
+   public function getSort()
+   {
+      return $this->sort;
+   }
+
+   /**
+    * Задать порядковый номер этапа
+    * @param int $sort
+    * @return  self
+    */
+   public function setSort($sort)
+   {
+      if (!is_numeric($sort)) {
+         throw new AmoCRMException('Передаваемая переменная не является числом');
+      }
+
+      $this->sort = $sort;
+
+      return $this;
+   }
+
+   /**
+    * Возвращает можно ли редактировать этап
+    * @return bool
+    */
+   public function getIsEditable()
+   {
+      return $this->is_editable;
+   }
+
+   /**
+    * Задает можно ли редактировать этап
+    * @param bool $is_editable
+    * @return  self
+    */
+   public function setIsEditable($is_editable)
+   {
+      if (!is_bool($is_editable)) {
+         throw new AmoCRMException('Передаваемая переменная не является булевой');
+      }
+
+      $this->is_editable = $is_editable;
+
       return $this;
    }
 }
