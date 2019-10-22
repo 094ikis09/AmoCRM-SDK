b5b44fadee067930fed2b4484e4be2e65a0e535d
Валерий <094ikis09@gmaol.com>
2019-10-22 17:37:27 +0300

    S

--- src/Models/NoteModel.php
+++ src/Models/NoteModel.php
@@ -22,4 +22,28 @@ class NoteModel extends BaseModel
       }
       return $this->client->call('/api/v2/notes', array(), $temp);
    }
+
+   /**
+    * Undocumented function
+    *
+    * @param [type] $type
+    * @param [type] $element_id
+    * @return NoteEntity[]
+    */
+   public function getNotes($type = null, $element_id = null)
+   {
+      $temp = $this->client->call(
+         '/api/v2/notes',
+         array(
+            'type' => $type,
+            'element_id' => $element_id
+         )
+      );
+
+      $notes = array();
+      foreach ($temp['_embedded']['items'] as $item) {
+         $notes[] = new NoteEntity($item);
+      }
+      return $notes;
+   }
 }
