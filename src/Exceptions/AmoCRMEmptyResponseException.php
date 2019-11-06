59456e551935be927029575a8b2bcf05977b0e3f
Валерий <094ikis09@gmaol.com>
2019-11-06 11:43:17 +0300

    Remove All

--- src/Exceptions/AmoCRMEmptyResponseException.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace AmoCRM\Exceptions;
-
-class AmoCRMEmptyResponseException extends AmoCRMException
-{ }
--- src/Exceptions/AmoCRMIoException.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace AmoCRM\Exceptions;
-
-class AmoCRMIoException extends AmoCRMException
-{ }
