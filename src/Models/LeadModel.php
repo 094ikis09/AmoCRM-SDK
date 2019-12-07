29be3cace1e8ae7a138fde8aa2d1295981d30124
Валерий <094ikis09@gmaol.com>
2019-12-07 21:48:33 +0300

    Update Remove Lead Method

--- src/Models/LeadModel.php
+++ src/Models/LeadModel.php
@@ -7,100 +7,100 @@ use AmoCRM\Exceptions\AmoCRMException;
 
 class LeadModel extends BaseModel
 {
-   /**
-    * Undocumented function
-    *
-    * @param integer $limitRows
-    * @param integer $limitOffset
-    * @param string $query
-    * @param string $with
-    * @param array $status
-    * @param array $filter
-    * @return LeadEntity[]
-    */
-   public function getLeads($limitRows = 500, $limitOffset = 0, $query = '', $with = '', $status = array(), $filter = array())
-   {
-      $leads = $this->client->call(
-         '/api/v2/leads',
-         'GET',
-         true,
-         false,
-         false,
-         array(
-            'limit_rows' => $limitRows,
-            'limit_offset' => $limitOffset,
-            'query' => $query,
-            'with' => $with,
-            'status' => $status,
-            'filter' => $filter
-         )
-      );
-      $temp = array();
-      if (isset($leads['_embedded']['items'])) {
-         foreach ($leads['_embedded']['items'] as $item) {
-            $temp[] = new LeadEntity($item);
-         }
-      }
-      return $temp;
-   }
-
-   public function createLeads($leads)
-   {
-      $temp['add'] = array();
-      if (is_array($leads)) {
-         foreach ($leads as $item) {
-            if ($item instanceof LeadEntity) {
-               $temp['add'][] = $item->generateQuery();
-            } else {
-               throw new AmoCRMException('Указан не вернный параметр');
+    /**
+     * Undocumented function
+     *
+     * @param integer $limitRows
+     * @param integer $limitOffset
+     * @param string $query
+     * @param string $with
+     * @param array $status
+     * @param array $filter
+     * @return LeadEntity[]
+     */
+    public function getLeads($limitRows = 500, $limitOffset = 0, $query = '', $with = '', $status = array(), $filter = array())
+    {
+        $leads = $this->client->call(
+            '/api/v2/leads',
+            'GET',
+            true,
+            false,
+            false,
+            array(
+                'limit_rows' => $limitRows,
+                'limit_offset' => $limitOffset,
+                'query' => $query,
+                'with' => $with,
+                'status' => $status,
+                'filter' => $filter
+            )
+        );
+        $temp = array();
+        if (isset($leads['_embedded']['items'])) {
+            foreach ($leads['_embedded']['items'] as $item) {
+                $temp[] = new LeadEntity($item);
             }
-         }
-      } else {
-         $temp['add'][] = $leads->generateQuery();
-      }
-      return $this->client->call('/api/v2/leads', array(), $temp);
-   }
+        }
+        return $temp;
+    }
 
-   public function updateLeads($leads)
-   {
-      $temp['update'] = array();
-      if (is_array($leads)) {
-         foreach ($leads as $item) {
-            if ($item instanceof LeadEntity) {
-               $temp['update'][] = $item->generateQuery();
-            } else {
-               throw new AmoCRMException('Указан не вернный параметр');
+    public function createLeads($leads)
+    {
+        $temp['add'] = array();
+        if (is_array($leads)) {
+            foreach ($leads as $item) {
+                if ($item instanceof LeadEntity) {
+                    $temp['add'][] = $item->generateQuery();
+                } else {
+                    throw new AmoCRMException('Указан не вернный параметр');
+                }
             }
-         }
-      } else {
-         $temp['update'][] = $leads->generateQuery();
-      }
-      return $this->client->call('/api/v2/leads', 'GET', true, false, false, array(), $temp);
-   }
+        } else {
+            $temp['add'][] = $leads->generateQuery();
+        }
+        return $this->client->call('/api/v2/leads', array(), $temp);
+    }
 
-   public function getLeadById($id)
-   {
-      $lead = $this->client->call('/api/v2/leads', 'GET', true, false, false, array('id' => $id));
-      return new LeadEntity($lead['_embedded']['items'][0]);
-   }
-
-   public function removeLeads($leads)
-   {
-      $temp['request']['multiactions']['add'][0] = array();
-      if (is_array($leads)) {
-         foreach ($leads as $item) {
-            if ($item instanceof LeadEntity) {
-               $temp['request']['multiactions']['add'][0]['ids'][] = $item->getId();
-            } else {
-               throw new AmoCRMException('Указан не вернный параметр');
+    public function updateLeads($leads)
+    {
+        $temp['update'] = array();
+        if (is_array($leads)) {
+            foreach ($leads as $item) {
+                if ($item instanceof LeadEntity) {
+                    $temp['update'][] = $item->generateQuery();
+                } else {
+                    throw new AmoCRMException('Указан не вернный параметр');
+                }
             }
-         }
-      } else {
-         $temp['request']['multiactions']['add'][0]['ids'][] = $leads->getId();
-      }
-      $temp['request']['multiactions']['add'][0]['data']['data']['ACTION'] = 'DELETE';
-      $temp['request']['multiactions']['add'][0]['entity_type'] = 2;
-      $temp['request']['multiactions']['add'][0]['multiaction_type'] = 4;
-      return $this->client->call('/ajax/v1/multiactions/set', 'POST', true, false, array(), $temp);
-   }
+        } else {
+            $temp['update'][] = $leads->generateQuery();
+        }
+        return $this->client->call('/api/v2/leads', 'GET', true, false, false, array(), $temp);
+    }
+
+    public function getLeadById($id)
+    {
+        $lead = $this->client->call('/api/v2/leads', 'GET', true, false, false, array('id' => $id));
+        return new LeadEntity($lead['_embedded']['items'][0]);
+    }
+
+    public function removeLeads($leads)
+    {
+        $temp['request']['multiactions']['add'][0] = array();
+        if (is_array($leads)) {
+            foreach ($leads as $item) {
+                if ($item instanceof LeadEntity) {
+                    $temp['request']['multiactions']['add'][0]['ids'][] = $item->getId();
+                } else {
+                    throw new AmoCRMException('Указан не вернный параметр');
+                }
+            }
+        } else {
+            $temp['request']['multiactions']['add'][0]['ids'][] = $leads->getId();
+        }
+        $temp['request']['multiactions']['add'][0]['data']['data']['ACTION'] = 'DELETE';
+        $temp['request']['multiactions']['add'][0]['entity_type'] = 2;
+        $temp['request']['multiactions']['add'][0]['multiaction_type'] = 4;
+        return $this->client->call('/ajax/v1/multiactions/set', 'POST', true, true, false, array(), $temp);
+    }
 }
